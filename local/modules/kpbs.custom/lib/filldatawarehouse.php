2984e77fc02158dcc18069ab97754d78ffbae768
Miros <lya-job@yandex.ru>
2021-03-13 16:34:30 +0300

    показатели кроме пульса

--- local/modules/kpbs.custom/lib/filldatawarehouse.php
+++ local/modules/kpbs.custom/lib/filldatawarehouse.php
@@ -23,6 +23,7 @@ class filldatawarehouse
         $curyear = date('Y');
         $curweekday = date("w");
         $curweek = date("W");
+        $terminatedate = date("d.m.Y", strtotime('31.12.2020'));
 
         echo $list;
         echo $curmonthdate;
@@ -36,12 +37,24 @@ class filldatawarehouse
             $countdeals = 0;
             $countmoddeals = 0;
             $countopen = 0;
+            // анализ сделок
+            $arResDeals = array();
 
-            $arFilter = array('ASSIGNED_BY_ID'=> $arResUser['ID']);
+            $arFilter = array('ASSIGNED_BY_ID'=> $arResUser['ID'], 'CLOSED'=>'Y', '>CLOSEDATE'=>$terminatedate);
             $arSelect = array('ID', 'ASSIGNED_BY_ID', 'CLOSED', 'CLOSEDATE', 'DATE_MODIFY',  $kbuf);
-            //$arSelect = array('*',  $kbuf);
             $obResDeal = \CCrmDeal::GetListEx(false,$arFilter,false,false,$arSelect);
-            while ($arResDeal = $obResDeal->Fetch()) {
+            while ($arResDealfirst = $obResDeal->Fetch()) {
+                array_push($arResDeals, $arResDealfirst);
+            }
+
+            $arFilter = array('ASSIGNED_BY_ID'=> $arResUser['ID'], 'CLOSED'=>'N');
+            $arSelect = array('ID', 'ASSIGNED_BY_ID', 'CLOSED', 'CLOSEDATE', 'DATE_MODIFY',  $kbuf);
+            $obResDeal = \CCrmDeal::GetListEx(false,$arFilter,false,false,$arSelect);
+            while ($arResDealsecond = $obResDeal->Fetch()) {
+                array_push($arResDeals, $arResDealsecond);
+            }
+
+            foreach ($arResDeals as $arResDeal) {
                 if($arResDeal['CLOSED']!='Y') {
                     $kvcurr += $arResDeal[$kbuf];
                     $kvopen += $arResDeal[$kbuf];
@@ -56,28 +69,43 @@ class filldatawarehouse
                     }
                 }
 
-                if($curweekday == 6) {
-                    $modweek = date('W', strtotime($arResDeal['DATE_MODIFY']));
-                    $modweekday = date('w', strtotime($arResDeal['DATE_MODIFY']));
-                    if ($curweek == $modweek) {
-                       if($modweekday > 1 && $modweekday < 6) {
-                           $countmoddeals++;
-                       }
+                if($curweekday == 5) {
+                    $compdate = strtotime('-3 days');
+                    if(strtotime($arResDeal['DATE_MODIFY']) > $compdate) {
+                        $countmoddeals++;
                     }
-
+                } elseif($curweekday == 2) {
+                    $compdate = strtotime('-4 days');
+                    if(strtotime($arResDeal['DATE_MODIFY']) > $compdate) {
+                        $countmoddeals++;
+                    }
                 }
-
-
-                echo "<pre>";
+                /*echo "<pre>";
                 print_r($arResDeal);
-                echo "</pre>";
+                echo "</pre>";*/
             }
             $kvavg = round($kvcurr / $countdeals,2);
+
+
+            // анализ компаний
+            $cntnetwork = 0;
+            $cntlevel = 0;
+            $countcnt = 0;
+            $arFilter = array('ASSIGNED_BY_ID'=> $arResUser['ID']);
+            $arSelect = array('ID', 'ASSIGNED_BY_ID', $cnuf, $cluf);
+            $obResCompany = \CCrmCompany::GetListEx(false,$arFilter,false,false,$arSelect);
+            while ($arResCompany = $obResCompany->Fetch()) {
+                $countcnt++;
+                $cntnetwork += $arResCompany[$cnuf];
+                $cntlevel += $arResCompany[$cluf];
+            }
+
+            // заполнение показателей
             $add = new \CIBlockElement();
             if($countdeals > 0) {
-                echo "<pre>";
+                /*echo "<pre>";
                 print_r($arResUser['ID']);
-                echo "</pre>";
+                echo "</pre>";*/
                 $data = [
                     'IBLOCK_ID' => $list,
                     'ACTIVE' => 'Y',
@@ -109,7 +137,7 @@ class filldatawarehouse
                     $data = [
                         'IBLOCK_ID' => $list,
                         'ACTIVE' => 'Y',
-                        'NAME' => 'KVavg',
+                        'NAME' => 'KVq',
                         'PROPERTY_VALUES' => [
                             'DATA_POKAZ'=> $curdate,
                             'MENEDZHER'=> $arResUser['ID'],
@@ -119,7 +147,7 @@ class filldatawarehouse
 
                     $id = $add->Add($data);
                 }
-                echo "<pre>";
+                /*echo "<pre>";
                 print_r($countopen);
                 echo "</pre>";
                 echo "<pre>";
@@ -133,18 +161,46 @@ class filldatawarehouse
                 echo "</pre>";
                 echo "<pre>";
                 print_r($kvavg);
-                echo "</pre>";
+                echo "</pre>";*/
             }
-            if($countopen>0) {
+            if ($countopen>0 && $countmoddeals>0) {
                 $qualact = round($countmoddeals / $countopen,2);
                 $data = [
                     'IBLOCK_ID' => $list,
                     'ACTIVE' => 'Y',
-                    'NAME' => 'KVcurr',
+                    'NAME' => 'QualAct',
                     'PROPERTY_VALUES' => [
                         'DATA_POKAZ'=> $curdate,
                         'MENEDZHER'=> $arResUser['ID'],
                         'ZNACHENIE_POKAZATELYA'=> $qualact
+                    ]
+                ];
+                $id = $add->Add($data);
+            }
+
+            if ($countcnt>0) {
+                $cntlevelavg = round($cntlevel / $countcnt,2);
+                $cntnetwavg = round($cntnetwork / $countcnt,2);
+                $data = [
+                    'IBLOCK_ID' => $list,
+                    'ACTIVE' => 'Y',
+                    'NAME' => 'CNTLev',
+                    'PROPERTY_VALUES' => [
+                        'DATA_POKAZ'=> $curdate,
+                        'MENEDZHER'=> $arResUser['ID'],
+                        'ZNACHENIE_POKAZATELYA'=> $cntlevelavg
+                    ]
+                ];
+                $id = $add->Add($data);
+
+                $data = [
+                    'IBLOCK_ID' => $list,
+                    'ACTIVE' => 'Y',
+                    'NAME' => 'CNTNet',
+                    'PROPERTY_VALUES' => [
+                        'DATA_POKAZ'=> $curdate,
+                        'MENEDZHER'=> $arResUser['ID'],
+                        'ZNACHENIE_POKAZATELYA'=> $cntnetwavg
                     ]
                 ];
                 $id = $add->Add($data);
