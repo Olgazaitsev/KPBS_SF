635414d72ab5e081d4fe23f34eb0b56ac4cc79e5
Miros <lya-job@yandex.ru>
2021-03-15 19:00:36 +0300

    эксперимент со своим классом

--- local/class/Kpbsclasses/TestClass.php
+++ local/class/Kpbsclasses/TestClass.php
@@ -7,8 +7,20 @@ class TestClass
 
     public static function init()
     {
+        \Bitrix\Main\Loader::includeModule('main');
+        if(\CModule::IncludeModule("crm"))
+        {
+            $arFilter = array('CLOSED'=>'Y');
+            $arSelect = array('ID', 'ASSIGNED_BY_ID', 'CLOSED', 'CLOSEDATE', 'DATE_MODIFY');
+            $obResDeal = \CCrmDeal::GetListEx(false,$arFilter,false,false,$arSelect);
+            while ($arResDealfirst = $obResDeal->Fetch()) {
+                echo "<pre>";
+                print_r($arResDealfirst);
+                echo "</pre>";
+            }
+        }
 
-        echo "okay";
+
 
     }
 
